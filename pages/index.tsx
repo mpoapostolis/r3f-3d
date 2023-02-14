134ab1d798b43352710582bba8d7f3b1aa48dbe9
mpoapostolis <mpoapostolis@gmail.com>
2023-02-15 00:24:39 +0200

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
-import { Environment, OrbitControls, useFBX } from "@react-three/drei";
+import { Hero } from "@/components/hero";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useRouter } from "next/router";
 
-function Hero() {
-  const fbx = useFBX("/models/dance.fbx");
-  fbx.traverse((child) => {
-    console.log(child);
-  });
-  return (
-    <mesh position={[0, 0, -5]} scale={[0.5, 0.5, 0.5]}>
-      <primitive object={fbx} />;
-    </mesh>
-  );
-}
+// function Hero() {
+//   const fbx = useGLTF("/models/hero.gltf");
+//   console.log(fbx);
+//   return <primitive object={fbx} />;
+// }
 
 export default function Admin() {
   const router = useRouter();
@@ -22,7 +17,6 @@ export default function Admin() {
         <Environment preset="park" background />
         <Hero />
         <OrbitControls />
-        <gridHelper position={[0, -2, 0]} args={[100, 100, 20, 20]} />
       </Canvas>
     </div>
   );
